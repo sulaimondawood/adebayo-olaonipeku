197813868800b132c8a6b0ddcdded2cd12103a39
sulaimondawood <sulaimondawod@gmail.com>
2023-04-12 01:38:30 +0100

    working on hero section

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -4,9 +4,18 @@ const Hero = () => {
   return (
     <>
       <div className={styles.hero_wrapper}>
-        <h1 className={styles.title}>
-          I develop innovative and intuitive softwares.
-        </h1>
+        <div className={styles.contact}>
+          <p>contact me</p>
+          <a href="'mailto:sulaimondawod@gmail.com">hohn@gmail.com</a>
+        </div>
+        <div className={styles.hero_wrapper_desc}>
+          <h1 className={styles.title}>Founder & Creative of Studio 28k</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit cumque
+            corporis similique quia suscipit nesciunt iusto porro veritatis
+            doloremque asperiores!
+          </p>
+        </div>
       </div>
     </>
   );
